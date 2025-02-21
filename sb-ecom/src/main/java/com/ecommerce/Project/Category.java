b65ac75cb7b67f6d2b19ed90672b2f2b2b74d1d2
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2025-02-22 00:19:33 +0530

    Update Category.java

--- sb-ecom/src/main/java/com/ecommerce/Project/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/Project/Category.java
@@ -1,16 +1,22 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.ecommerce.Project;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity(name = "categories")// to make table ,, default db name - Category., if not mention name
+@Entity(
+    name = "categoriies"
+)
 public class Category {
-    @Id//primary key
+    @Id
     private long CategoryId;
     private String CategoryName;
 
-
-    public Category(long Categoryid , String categoryname) {
+    public Category(long Categoryid, String categoryname) {
         this.CategoryId = Categoryid;
         this.CategoryName = categoryname;
     }
@@ -19,18 +25,18 @@ public class Category {
     }
 
     public long getCategoryId() {
-        return CategoryId;
+        return this.CategoryId;
     }
 
     public void setCategoryId(long categoryId) {
-        CategoryId = categoryId;
+        this.CategoryId = categoryId;
     }
 
     public String getCategoryName() {
-        return CategoryName;
+        return this.CategoryName;
     }
 
     public void setCategoryName(String categoryName) {
-        CategoryName = categoryName;
+        this.CategoryName = categoryName;
     }
 }
