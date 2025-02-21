96f484e9c17b22196085a23b6901d8acb3a9e5d6
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2025-02-22 00:27:19 +0530

    Update CategoryRepository.java

--- sb-ecom/src/main/java/com/ecommerce/Project/Repo/CategoryRepository.java
+++ sb-ecom/src/main/java/com/ecommerce/Project/Repo/CategoryRepository.java
@@ -1,1 +1,8 @@
+package com.ecommerce.Project.repositories;
+
+import com.ecommerce.Project.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
 
